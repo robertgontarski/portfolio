2fcdb848d1a89a95ead0f7a753d752346204adc7
robert.gontarski <gontrebor@gmail.com>
2023-11-01 03:03:12 +0100

    remove darkmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,14 @@
 
 import './globals.css'
 import React, { type ReactNode } from 'react'
-import { useTheme } from '@/components/hooks/useTheme'
 
 export default function RootLayout ({
   children
 }: {
   children: ReactNode
 }): ReactNode {
-  const isDarkMode = useTheme()
-
   return (
-    <html data-mode={isDarkMode ? 'dark' : 'light'}>
+    <html data-mode="light">
       <body className="bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-100 font-medium">
       <div className="max-w-2xl mx-auto my-16 md:my-28 px-4">
         {children}
